f0d94103bbc727d8cd32b2ddbe71536d9b005c35
Omar Iglesk <marloo-_-@hotmail.com>
2018-10-12 17:34:03 +0300

    Add project files.

---
+++ LMS_MiniProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS_MiniProject.Startup))]
+namespace LMS_MiniProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
